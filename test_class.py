ed29d5922d3dfcd23edfeac1ce18b1d81632f7cc
ruralkan <ruralkan@gmail.com>
2019-07-28 00:26:38 -0500

    Group multiple tests in a class fail

---
+++ test_class.py
@@ -0,0 +1,10 @@
+# content of test_class.py
+# pytest test_class.py
+class TestClass(object):
+    def test_one(self):
+        x = "this"
+        assert 'h' in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, 'check')
